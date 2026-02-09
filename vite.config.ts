cb1fc845971dbcbfbc8f3a08077097dea9d59eae
Bijay Rouniyar <110227776+BijjuVaiii@users.noreply.github.com>
2026-02-09 16:24:06 +0530

    vite file uploaded

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/",
+  build: {
+    outDir: "docs", // ✅ REQUIRED for GitHub Pages
+    emptyOutDir: true,
+  },
   server: {
     host: "::",
     port: 8080,
